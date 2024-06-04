19ac9c3f07721febdaa0ffbd2f9e2fe6873f82ec
Marco Aurélio de S Cabral <marcoascabral@gmail.com>
2024-06-04 09:08:15 -0300

    1st Commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+        <div>
+            <span class="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><!-- ... --></svg>
+            </span>
+        </div>
+        <h3 class="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Módulo Base</h3>
+        <p class="text-slate-500 dark:text-slate-400 mt-2 text-sm">
+            O Módulo Base é um projeto Nuxt com os componentes necessários para iniciar um projeto com as seguintes configurações: Nuxt + Tailwind + Pinia
+    
+        </p>
+        <NuxtLink to="/base">Link to another page</NuxtLink>
+    </div>
+  </template>
